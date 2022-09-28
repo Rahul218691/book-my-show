a87332b1a5d5f422362b106f9f3dad16a1b402ea
Rahul Guptha <49743998+Rahul218691@users.noreply.github.com>
2022-09-28 11:01:49 +0530

    added preloader

---
+++ client/src/components/spinners/Pageloader/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+const PageLoader = () => {
+  return (
+<div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  )
+}
+
+export default PageLoader
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
+import { PageLoader } from './components'
 const App = lazy(() => import('./App'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Suspense fallback={<h1>Loading...</h1>}>
+    <Suspense fallback={<PageLoader />}>
         <App />
     </Suspense>
 );
